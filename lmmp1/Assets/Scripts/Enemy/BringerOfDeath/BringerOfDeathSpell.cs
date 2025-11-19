ecef5030073af61523d4b03314448e2aa7915272
btwcerf16 <btwcerf14506@gmail.com>
2025-11-19 23:57:01 +0300

    fix

--- lmmp1/Assets/Scripts/Enemy/BringerOfDeath/BringerOfDeathSpell.cs
+++ lmmp1/Assets/Scripts/Enemy/BringerOfDeath/BringerOfDeathSpell.cs
@@ -6,14 +6,17 @@ public class BringerOfDeathSpell : MonoBehaviour
 {
   
 
-    public List<Transform> SpellPoints;
+    [SerializeField]private Transform SpellPoint;
 
-    public GameObject SpellPrefab;
+    [SerializeField]private GameObject SpellPrefab;
 
     [SerializeField]private Transform DomainParent;
 
+    [SerializeField] private float SpellRadius;
     private void CastSpell()
     {
-        Instantiate(SpellPrefab, SpellPoints[Random.Range(0, SpellPoints.Count)].position,Quaternion.Euler(0f,0f,0f), DomainParent);
+        Vector2 randomPointInCircle = Random.insideUnitCircle * SpellRadius;
+        Vector3 instantiatePos = new Vector3(randomPointInCircle.x * SpellRadius + SpellPoint.position.x, randomPointInCircle.y * SpellRadius + SpellPoint.position.y, 0.0f);
+        Instantiate(SpellPrefab,instantiatePos ,Quaternion.Euler(0f,0f,0f), DomainParent);
     }
 }
